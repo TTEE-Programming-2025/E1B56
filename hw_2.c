1dc536d89872af4b9681177911a7c80a7680234c
E1B56 <u11302256@o365.ttu.edu.tw>
2025-04-19 15:00:29 +0800

    第一次上傳檔案有誤

--- hw_2.c
+++ hw_2.c
@@ -1,14 +1,32 @@
 #include<stdlib.h>
 #include<stdio.h>
 int main(){
-	int password,correct=0,i;
+	int password,correct=0,i,a;
+	const int real_password=2025;
 	
 	//個人風格畫面 
-	for(i=1;i<=20;i++){
+	for(i=0;i<=20;i++){
 		printf("*****           *****\n");
 	}
-	
-	
+	printf("密碼預設:2025\n");
+	//密碼驗證 
+	for(a=1;a<=3;a++){
+		printf("輸入密碼:");
+		scanf("%d",&password);		
+		if(password==real_password){
+			printf("密碼正確!\n");
+			correct=1;
+			break;
+		}
+		else{
+			correct=0;
+			printf("密碼錯誤!");
+		}
+}
+	if(correct==0){
+		printf("密碼錯誤三次!\n");
+		return 0;
+	}
 	
 	
 	
