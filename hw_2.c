d1ac59ea21090a7b2090a7f5e02ed2a322f317b1
E1B56 <u11302256@o365.ttu.edu.tw>
2025-04-19 14:35:20 +0800

    第一題的第一次測試

---
+++ hw_2.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+	int password,correct=0,i;
+	
+	//個人風格畫面 
+	for(i=1;i<=20;i++){
+		printf("*****           *****\n");
+	}
+	
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
