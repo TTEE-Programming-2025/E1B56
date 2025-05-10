e1c64b1151cc901ad10c261c7eb346aadddc86bb
E1B56 <u11302256@o365.ttu.edu.tw>
2025-05-11 02:03:33 +0800

    I changed the original Chinese part into English and made a seating chart for the third question.

--- hw_3.c
+++ hw_3.c
@@ -1,38 +1,43 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<time.h>
+#define row 9  
+#define col 9
+	char seat[row][col];
 int main(){
 	int a,password,i,correct;
 	const int real_password=2025;
 
-//個人風格畫面 
+//personal style screen
 	for(i=0;i<=20;i++){
 		printf("*****           *****\n");
 	}
 
-//密碼驗證 
+//Password verification
 	for(a=1;a<=3;a++){
-		printf("輸入密碼:");
+		printf("Enter password:");
 		scanf("%d",&password);		
 		if(password==real_password){
-			printf("密碼正確!\n");
+			printf("Password is correct!\n");
 			correct=1;
 			break;
 		}
 		else{
 			correct=0;
-			printf("密碼錯誤!\n");
+			printf("Wrong password!\n");
 		}
 }
 if(correct==0){
-		printf("密碼錯誤三次!\n");
+		printf("Wrong password three times!\n");
 		return 0;
 	}
 	clearscreen();
     menu();
+    initializeSeats();
     
     return 0;
 }
-
+//Main menu
 void menu(){
 	printf("----------[Booking System]----------\n");
     printf("| a. Available seats                |\n");
@@ -44,3 +49,30 @@ void menu(){
 void clearscreen(){
 	system("cls");
 }
+void initializeSeats(){
+    int i,j,r,c;
+    for (i=0;i<row;i++) {
+        for (j=0;j<col;j++) {
+            seat[i][j]='-';
+        }
+    }
+    srand(time(0));
+    for (i=0;i<10;i++) {
+        r=rand()%row;
+        c=rand()%col;
+        seat[r][c]='*';
+    }
+    displaySeats();
+}
+void displaySeats(){
+    int i,j;
+    printf("123456789\n");
+    for (i=0;i<row;i++) {
+        printf("%d",row-i);
+        for (j=0;j<col;j++) {
+            printf("%c",seat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
