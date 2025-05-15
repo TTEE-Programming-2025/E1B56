bd2b234b7e4afdc79c5fca0ff15128ef65c04486
E1B56 <u11302256@o365.ttu.edu.tw>
2025-05-15 23:00:56 +0800

    完成了主選單和清除螢幕的函式

--- hw_4.c
+++ hw_4.c
@@ -1,11 +1,29 @@
 #include<stdlib.h>
 #include<stdio.h>
 #define password 2025
+
+//清除螢幕
+void clearscreen(){
+	system("cls");
+} 
+// 主選單
+void menu(){
+    printf("------------[Grade System]----------\n");
+    printf("|  a. Enter student grades         |\n");
+    printf("|  b. Display student grades       |\n");
+    printf("|  c. Search for student grades    |\n");
+    printf("|  d. Grade ranking                |\n");
+    printf("|  e. Exit system                  |\n");
+    printf("------------------------------------\n");
+    printf("請選擇功能:");
+}
+
 int main(){
 	int input_password,a=0,i;
 	// 個人風格畫面
     for(i=0;i<20;i++)
         printf("***** 歡迎使用 *****\n");
+    //密碼驗證 
     while(a<3){
     	printf("輸入密碼:");
     	scanf("%d",&input_password);
@@ -22,6 +40,8 @@ int main(){
 		printf("密碼錯誤三次，結束!");
 		return 0; 
 	}
+	clearscreen();
+	menu();
 	
 	return 0;
 }
